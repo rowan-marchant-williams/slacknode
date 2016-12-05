bd7c51c94ba34f6bcd7c75abb945607cb62d7bf3
rowan-marchant-williams-i20 <rowan.marchant-williams@i2owater.com>
2016-12-05 16:03:15 +0000

    init

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+var fs = require("fs");
+
+var bodyParser = require('body-parser')
+app.use( bodyParser.json() );
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.post('/slackevent', function (req, res) {
+    console.log( req.body );
+    res.status(200);
+})
+
+var server = app.listen(4005, function () {
+
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log("Example app listening at http://%s:%s", host, port)
+
+})
