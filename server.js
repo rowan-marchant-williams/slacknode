5eb8c63d4a764c3e34c2f6e1dfdcb4002334dcb7
rowan-marchant-williams-i20 <rowan.marchant-williams@i2owater.com>
2016-12-06 11:49:49 +0000

    Adjusted port

--- server.js
+++ server.js
@@ -2,22 +2,26 @@ var express = require('express');
 var app = express();
 var fs = require("fs");
 
+var port = process.env.port || 1337
+
 var bodyParser = require('body-parser')
 app.use( bodyParser.json() );
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
 app.post('/slackevent', function (req, res) {
     console.log( req.body );
     res.status(200);
 })
 
-var server = app.listen(80, function () {
+var server = app.listen(port, function () {
+    var host = server.address().address;
+    var port = server.address().port;
 
-    var host = server.address().address
-    var port = server.address().port
-
-    console.log("Example app listening at http://%s:%s", host, port)
-
-})
+    console.log('Example app listening at http://%s:%s', host, port);
+});
